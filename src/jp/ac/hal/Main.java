cb1a3aae05d9094976bb51d5a4a4855ec56b6db9
ryoki726 <ryooki.0726@gmail.com>
2019-07-24 16:54:38 +0900

    抽象クラスを作る

--- src/jp/ac/hal/Main.java
+++ src/jp/ac/hal/Main.java
@@ -1,11 +1,13 @@
 package jp.ac.hal;
 
+//抽象クラス
+abstract class Figure{
+	abstract void area(); //これが面積
+	abstract void around(); //これが外周
 
-public class Main {
-
-	public static void main(String[] args) {
-
-
+	void measure() {
+		area();
+		around();
+		System.out.println();
 	}
-
 }
