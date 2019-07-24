16d5308002aa297f0ee35b594462d9063b320598
ryoki726 <ryooki.0726@gmail.com>
2019-07-24 17:05:01 +0900

    面積、外周を求める時に必要なものと式

--- src/jp/ac/hal/Main.java
+++ src/jp/ac/hal/Main.java
@@ -11,3 +11,26 @@ abstract class Figure{
 		System.out.println();
 	}
 }
+
+//抽象クラスのサブクラス
+class Square extends Figure{
+	double side;
+	double height;
+
+	Square(double s){ //コンストラクタ
+		side = height = s;
+	}
+
+	Square(double s, double h){
+		side = s;
+		height = h;
+	}
+
+	void area(){ //抽象メソッドのオーバーライド
+		System.out.println("四角の面積:"+ (side * height));
+	}
+
+	void around(){ //抽象メソッドのオーバーライド
+		System.out.println("四角の外周:"+ (2 * (side * height)));
+	}
+}
